cf0602716f55e830f81c8b06d199f25778ffa6a2
mathweuzz <mathweuzz@gmail.com>
2023-04-14 09:42:38 -0300

    some changes

--- models.py
+++
@@ -1,8 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
-
-class Presenca(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    matricula = db.Column(db.String(20), nullable=False)
-    datahora = db.Column(db.DateTime, nullable=False)
--- utils.py
+++
@@ -1,5 +0,0 @@
-import datetime
-
-def formata_data():
-    data = datetime.datetime.now()
-    return data.strftime('%d/%m/%Y')
